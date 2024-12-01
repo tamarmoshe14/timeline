f29ac2c1e048405dddb3f36fb9fc2fc041506e15
Tamar Moshe <tamarm@brainpop.com>
2024-12-01 13:32:54 +0200

    add about page

--- timeline/pages/about/index.vue
+++ timeline/pages/about/index.vue
@@ -1,3 +1,31 @@
 <template>
-  <div>About</div>
+  <div class="about_container">
+    <h1 class="title">About</h1>
+    <p>This timeline is a tool for those of you who want to deepen their understanding of the sequence of events before, during or after the holocaust. In this timeline you will find events that are related to world war 2, the holocaust and many other topics that fall under these categories.</p>
+    <h2 class="title">How to use the timeline?</h2>
+    <ul class="list">
+      <li>The events are displayed on a timeline by the order of their date.</li>
+      <li>By clicking an event you'll be able to see a short description of the event. Some of the events will contain an image or a map as well.</li>
+      <li>You can filter the events on the timeline to suit your needs.</li>
+      <li>You can zoom in and out of the timeline to see more or less events at once.</li>
+    </ul>
+    <h2 class="title">Can I also upload events?</h2>
+    <p>Sure! Sign in to the website and once you'll get approved by the owners, you can add as many events as you want.</p>
+  </div>
 </template>
+
+<style scoped lang="scss">
+.about_container {
+  margin: 6rem auto;
+  width: 50rem;
+  text-align: center;
+
+  .list {
+    text-align: left;
+  }
+
+  .title {
+    margin: 2rem 0;
+  }
+}
+</style>
